99b0876f516176b26d361cd76071d271b738ebb7
Swarnatrisa Karmakar <swarnakar1009@gmail.com>
2026-01-03 18:34:26 +0530

    Undo changes

--- auth/nextjs/currentUser.ts
+++
@@ -1,8 +0,0 @@
-import { cache } from "react";
-import { getUserFromSession } from "../core/session";
-import { cookies } from "next/headers";
-
-export const getCurrentUser = cache(async () => {
-    // console.log(getUserFromSession(await cookies()))
-    return getUserFromSession(await cookies())
-})
